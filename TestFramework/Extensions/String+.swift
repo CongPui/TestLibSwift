391ea70accf93ff432695dce509eea78ebc95074
CongPui <138424374+CongPui@users.noreply.github.com>
2023-07-04 14:15:08 +0700

    create extension String, extension UIView

---
+++ TestFramework/Extensions/String+.swift
@@ -0,0 +1,8 @@
+//
+//  String+.swift
+//  TestFramework
+//
+//  Created by Cong Bui Thanh on 04/07/2023.
+//
+
+import Foundation
---
+++ TestFramework/Resource.swift
@@ -0,0 +1,8 @@
+//
+//  Resource.swift
+//  TestFramework
+//
+//  Created by Cong Bui Thanh on 04/07/2023.
+//
+
+import Foundation
